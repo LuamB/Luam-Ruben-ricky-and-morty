6b3ea188fbbe457d0591423a914ab3301d370916
Luam Belay <luam.belay@gmail.com>
2024-01-10 18:05:06 +0100

    search bar input

--- components/nav-pagination/nav-pagination.js
+++ components/nav-pagination/nav-pagination.js
@@ -1,8 +1,8 @@
-export function createPagination(page, maxPage) {
-  const pageNav = document.createElement("span");
-  pageNav.innerHTML = `<span class="navigation__pagination" data-js="pagination">${page} / ${maxPage}</span>`;
-  return pageNav;
-}
+// export function createPagination(page, maxPage) {
+//   const pageNav = document.createElement("span");
+//   pageNav.innerHTML = `<span class="navigation__pagination" data-js="pagination">${page} / ${maxPage}</span>`;
+//   return pageNav;
+// }
 
 // export function paginationDisplay(page, maxPage) {
 //   pagination.textContent = `${page} / ${maxPage}`;
