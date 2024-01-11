e4c0750c2e5e47fee938e8f125a9523cb9d92893
Luam Belay <luam.belay@gmail.com>
2024-01-12 00:18:03 +0100

    More refactoring

--- components/nav-button/nav-button.js
+++ components/nav-button/nav-button.js
@@ -1,28 +1,40 @@
 import { fetchCharacters } from "../../index.js";
 
-export function createButton() {
-  // create previous button element
+export function createButton(page, maxPage) {
+  // previous button element
   const prevButton = document.createElement("button");
   prevButton.classList.add("button--prev");
+  prevButton.textContent = "previous";
 
-  // create next button element
+  // next button element
   const nextButton = document.createElement("button");
   nextButton.classList.add("button--next");
+  nextButton.textContent = "next";
+
+  // add button functionality
+  //   function handlePrevButton(page) {
+  //     if (page > 1) {
+  //       page--;
+  //       fetchCharacters(page, searchQuery);
+  //     }
+  //   }
+
+  function handlePrevButton(page) {
+    page > 1 && (page--, fetchCharacters(page, searchQuery));
+  }
+
+  //   function handleNextButton(page, maxPage) {
+  //     if (page < maxPage) {
+  //       page++;
+  //       fetchCharacters(page, searchQuery);
+  //     }
+  //   }
+
+  function handleNextButton(page, maxPage) {
+    page < maxPage && (page++, fetchCharacters(page, searchQuery));
+  }
+  prevButton.addEventListener("click", handlePrevButton);
+  nextButton.addEventListener("click", handleNextButton);
 
   return [prevButton, nextButton];
-}
-
-export function handlePrevButton(page) {
-  if (page > 1) {
-    page--;
-    fetchCharacters(page);
-  }
-}
-
-export function handleNextButton(page, maxPage) {
-  console.log("page", page);
-  if (page < maxPage) {
-    page++;
-    fetchCharacters(page);
-  }
 }
--- components/nav-pagination/nav-pagination.js
+++ components/nav-pagination/nav-pagination.js
@@ -1,5 +1,3 @@
-// import { createButton } from "../nav-button/nav-button.js";
-
 export function createPagination(page, maxPage) {
   const pagination = document.createElement("span");
   pagination.classList.add("navigation__pagination");
